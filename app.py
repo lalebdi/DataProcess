f7ac3db2ec0920bf39485206b84fee312899da29
lalebdi <loai.alebdi@gmail.com>
2021-08-07 18:46:56 -0400

    process name and phone if blank

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_restful import Api, Resource, reqparse, abort
+from nameparser import HumanName
 
 app = Flask(__name__)
 api = Api(app)
@@ -10,6 +11,39 @@ post_args.add_argument("mode", type=str, help="Choose between phone || name || a
 post_args.add_argument("replace_with", type=str, help="Choose either --blank-- || --original--", required=True)
 
 
+def processing_data(data):
+    new_data = {}
+    if data["replace_with"] == "--blank--":
+        if data["mode"] == "name":
+            name = HumanName(data["value"])
+            cleaned_name = name_output_cleanup(name.as_dict())
+            new_data["output"] = cleaned_name
+        elif data["mode"] == "phone":
+            phone_num = extract_phone_number(data["value"])
+            new_data["output"] = phone_num
+        elif data["mode"] == "amount":
+            print("its amount")
+    elif data["replace_with"] == "--original--":
+        print("Its original")
+    return new_data
+
+
+def extract_phone_number(num):
+    phone_num = ""
+    for i in num:
+        if i.isdigit():
+            phone_num += i
+    return phone_num
+
+
+def name_output_cleanup(data):
+    name = dict()
+    for key, value in data.items():
+        if value != "":
+            name[key] = value
+    return name
+
+
 class DataProcess(Resource):
     def get(self):
         return {"message": 'This URL that will accept a POST call with the following payload : '
@@ -17,7 +51,8 @@ class DataProcess(Resource):
 
     def post(self):
         args = post_args.parse_args()
-        return args, 201
+        output_data = processing_data(args)
+        return output_data, 201
 
 
 api.add_resource(DataProcess, "/")
--- test.py
+++ test.py
@@ -5,16 +5,65 @@ BASE = "http://127.0.0.1:5000/"
 response = requests.get(BASE)
 print(response.json())
 
+# input()
+#
+# payload = {'value': 'value1', 'mode': 'value2', "replace_with": "null"}
+#
+# r = requests.post(BASE, data=payload)
+# print(r.json())
+
+# input()
+#
+# payload2 = {'value': 'value1', 'mode': 'value2', "wrong_key": "null"}
+#
+# r = requests.post(BASE, data=payload2)
+# print(r.json())
+
+# input()
+#
+# payload = {'value': 'value1', 'mode': 'value2', "replace_with": "--original--"}
+#
+# r = requests.post(BASE, data=payload)
+# print(r.json())
+#
+# input()
+#
+# payload = {'value': 'value1', 'mode': 'value2', "replace_with": "--blank--"}
+#
+# r = requests.post(BASE, data=payload)
+# print(r.json())
+
+# input()
+#
+# payload = {'value': 'value1', 'mode': 'name', "replace_with": "--blank--"}
+#
+# r = requests.post(BASE, data=payload)
+# print(r.json())
+#
+# input()
+#
+# payload = {'value': 'value1', 'mode': 'phone', "replace_with": "--blank--"}
+#
+# r = requests.post(BASE, data=payload)
+# print(r.json())
+#
+# input()
+#
+# payload = {'value': 'value1', 'mode': 'amount', "replace_with": "--blank--"}
+#
+# r = requests.post(BASE, data=payload)
+# print(r.json())
+
 input()
 
-payload = {'value': 'value1', 'mode': 'value2', "replace_with": "null"}
+payload = {"value": "(512) 234-9293", "mode": "phone", "replace_with": "--blank--"}
 
 r = requests.post(BASE, data=payload)
 print(r.json())
 
 input()
 
-payload2 = {'value': 'value1', 'mode': 'value2', "wrong_key": "null"}
+payload = {"value": "Robert Lance Smith", "mode": "name", "replace_with": "--blank--"}
 
-r = requests.post(BASE, data=payload2)
+r = requests.post(BASE, data=payload)
 print(r.json())

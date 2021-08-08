aea9da91e283d6beda301039c79ed5c7af3a2cf3
lalebdi <loai.alebdi@gmail.com>
2021-08-08 03:23:05 -0400

    fix the server error

--- mock.py
+++ mock.py
@@ -19,162 +19,162 @@ print("The payload", payload)
 r = requests.post(BASE, data=payload)
 print(r.json())
 
-# input("Press Enter for the next")
-#
-# payload = {'value': 'value1', 'mode': 'value2', "replace_with": "--original--"}
-# print("The payload", payload)
-# r = requests.post(BASE, data=payload)
-# print(r.json())
-#
-# input("Press Enter for the next")
-#
-# payload = {'value': 'value1', 'mode': 'value2', "replace_with": "--blank--"}
-# print("The payload", payload)
-# r = requests.post(BASE, data=payload)
-# print(r.json())
-#
-# input("Press Enter for the next")
-#
-# payload = {'value': 'value1', 'mode': 'name', "replace_with": "--blank--"}
-# print("The payload", payload)
-# r = requests.post(BASE, data=payload)
-# print(r.json())
-#
-# input("Press Enter for the next")
-#
-# payload = {'value': 'value1', 'mode': 'phone', "replace_with": "--blank--"}
-# print("The payload", payload)
-# r = requests.post(BASE, data=payload)
-# print(r.json())
-#
-# input("Press Enter for the next")
-#
-# payload = {'value': 'value1', 'mode': 'amount', "replace_with": "--blank--"}
-# print("The payload", payload)
-# r = requests.post(BASE, data=payload)
-# print(r.json())
-#
-# input("Press Enter for the next")
-#
-# payload = {"value": "(512) 234-9293", "mode": "phone", "replace_with": "--blank--"}
-# print("The payload", payload)
-# r = requests.post(BASE, data=payload)
-# print(r.json())
-#
-# input("Press Enter for the next")
-#
-# payload = {"value": "unknown", "mode": "phone", "replace_with": "--blank--"}
-# print("The payload", payload)
-# r = requests.post(BASE, data=payload)
-# print(r.json())
-#
-# input("Press Enter for the next")
-#
-# payload = {"value": "unknown", "mode": "phone", "replace_with": "--original--"}
-# print("The payload", payload)
-# r = requests.post(BASE, data=payload)
-# print(r.json())
-#
-# input("Press Enter for the next")
-#
-# payload = {"value": "Robert Lance Smith", "mode": "name", "replace_with": "--blank--"}
-# print("The payload", payload)
-# r = requests.post(BASE, data=payload)
-# print(r.json())
-#
-#
-# input("Press Enter for the next")
-#
-# payload = {"value": "12234", "mode": "name", "replace_with": "--blank--"}
-# print("The payload", payload)
-# r = requests.post(BASE, data=payload)
-# print(r.json())
-#
-#
-# input("Press Enter for the next")
-#
-# payload = {"value": "-12234", "mode": "name", "replace_with": "--blank--"}
-# print("The payload", payload)
-# r = requests.post(BASE, data=payload)
-# print(r.json())
-#
-#
-# input("Press Enter for the next")
-#
-# payload = {"value": "unknown", "mode": "name", "replace_with": "--blank--"}
-# print("The payload", payload)
-# r = requests.post(BASE, data=payload)
-# print(r.json())
-#
-#
-# input("Press Enter for the next")
-#
-# payload = {"value": " ", "mode": "name", "replace_with": "--blank--"}
-# print("The payload", payload)
-# r = requests.post(BASE, data=payload)
-# print(r.json())
-#
-# input("Press Enter for the next")
-#
-# payload = {"value": "Robert Lance Smith", "mode": "name", "replace_with": "--original--"}
-# print("The payload", payload)
-# r = requests.post(BASE, data=payload)
-# print(r.json())
-#
-#
-# input("Press Enter for the next")
-#
-# payload = {"value": "12234", "mode": "name", "replace_with": "--original--"}
-# print("The payload", payload)
-# r = requests.post(BASE, data=payload)
-# print(r.json())
-#
-#
-# input("Press Enter for the next")
-#
-# payload = {"value": "unknown", "mode": "name", "replace_with": "--original--"}
-# print("The payload", payload)
-# r = requests.post(BASE, data=payload)
-# print(r.json())
-#
-#
-# input("Press Enter for the next")
-#
-# payload = {"value": " ", "mode": "name", "replace_with": "--original--"}
-# print("The payload", payload)
-# r = requests.post(BASE, data=payload)
-# print(r.json())
-#
-#
-# input("Press Enter for the next")
-#
-# payload = {"value": "2 * 2", "mode": "amount", "replace_with": "--blank--"}
-# print("The payload", payload)
-# r = requests.post(BASE, data=payload)
-# print(r.json())
-#
-#
-# input("Press Enter for the next")
-#
-# payload = {"value": "3.14159 + 2", "mode": "amount", "replace_with": "--blank--"}
-# print("The payload", payload)
-# r = requests.post(BASE, data=payload)
-# print(r.json())
-#
-#
-# input("Press Enter for the next")
-#
-# payload = {"value": "dir()", "mode": "amount", "replace_with": "--blank--"}
-# print("The payload", payload)
-# r = requests.post(BASE, data=payload)
-# print(r.json())
-#
-#
-# input("Press Enter for the next")
-#
-# payload = {"value": "dir()", "mode": "amount", "replace_with": "--original--"}
-# print("The payload", payload)
-# r = requests.post(BASE, data=payload)
-# print(r.json())
+input("Press Enter for the next")
+
+payload = {'value': 'value1', 'mode': 'value2', "replace_with": "--original--"}
+print("The payload", payload)
+r = requests.post(BASE, data=payload)
+print(r.json())
+
+input("Press Enter for the next")
+
+payload = {'value': 'value1', 'mode': 'value2', "replace_with": "--blank--"}
+print("The payload", payload)
+r = requests.post(BASE, data=payload)
+print(r.json())
+
+input("Press Enter for the next")
+
+payload = {'value': 'value1', 'mode': 'name', "replace_with": "--blank--"}
+print("The payload", payload)
+r = requests.post(BASE, data=payload)
+print(r.json())
+
+input("Press Enter for the next")
+
+payload = {'value': 'value1', 'mode': 'phone', "replace_with": "--blank--"}
+print("The payload", payload)
+r = requests.post(BASE, data=payload)
+print(r.json())
+
+input("Press Enter for the next")
+
+payload = {'value': 'value1', 'mode': 'amount', "replace_with": "--blank--"}
+print("The payload", payload)
+r = requests.post(BASE, data=payload)
+print(r.json())
+
+input("Press Enter for the next")
+
+payload = {"value": "(512) 234-9293", "mode": "phone", "replace_with": "--blank--"}
+print("The payload", payload)
+r = requests.post(BASE, data=payload)
+print(r.json())
+
+input("Press Enter for the next")
+
+payload = {"value": "unknown", "mode": "phone", "replace_with": "--blank--"}
+print("The payload", payload)
+r = requests.post(BASE, data=payload)
+print(r.json())
+
+input("Press Enter for the next")
+
+payload = {"value": "unknown", "mode": "phone", "replace_with": "--original--"}
+print("The payload", payload)
+r = requests.post(BASE, data=payload)
+print(r.json())
+
+input("Press Enter for the next")
+
+payload = {"value": "Robert Lance Smith", "mode": "name", "replace_with": "--blank--"}
+print("The payload", payload)
+r = requests.post(BASE, data=payload)
+print(r.json())
+
+
+input("Press Enter for the next")
+
+payload = {"value": "12234", "mode": "name", "replace_with": "--blank--"}
+print("The payload", payload)
+r = requests.post(BASE, data=payload)
+print(r.json())
+
+
+input("Press Enter for the next")
+
+payload = {"value": "-12234", "mode": "name", "replace_with": "--blank--"}
+print("The payload", payload)
+r = requests.post(BASE, data=payload)
+print(r.json())
+
+
+input("Press Enter for the next")
+
+payload = {"value": "unknown", "mode": "name", "replace_with": "--blank--"}
+print("The payload", payload)
+r = requests.post(BASE, data=payload)
+print(r.json())
+
+
+input("Press Enter for the next")
+
+payload = {"value": " ", "mode": "name", "replace_with": "--blank--"}
+print("The payload", payload)
+r = requests.post(BASE, data=payload)
+print(r.json())
+
+input("Press Enter for the next")
+
+payload = {"value": "Robert Lance Smith", "mode": "name", "replace_with": "--original--"}
+print("The payload", payload)
+r = requests.post(BASE, data=payload)
+print(r.json())
+
+
+input("Press Enter for the next")
+
+payload = {"value": "12234", "mode": "name", "replace_with": "--original--"}
+print("The payload", payload)
+r = requests.post(BASE, data=payload)
+print(r.json())
+
+
+input("Press Enter for the next")
+
+payload = {"value": "unknown", "mode": "name", "replace_with": "--original--"}
+print("The payload", payload)
+r = requests.post(BASE, data=payload)
+print(r.json())
+
+
+input("Press Enter for the next")
+
+payload = {"value": " ", "mode": "name", "replace_with": "--original--"}
+print("The payload", payload)
+r = requests.post(BASE, data=payload)
+print(r.json())
+
+
+input("Press Enter for the next")
+
+payload = {"value": "2 * 2", "mode": "amount", "replace_with": "--blank--"}
+print("The payload", payload)
+r = requests.post(BASE, data=payload)
+print(r.json())
+
+
+input("Press Enter for the next")
+
+payload = {"value": "3.14159 + 2", "mode": "amount", "replace_with": "--blank--"}
+print("The payload", payload)
+r = requests.post(BASE, data=payload)
+print(r.json())
+
+
+input("Press Enter for the next")
+
+payload = {"value": "dir()", "mode": "amount", "replace_with": "--blank--"}
+print("The payload", payload)
+r = requests.post(BASE, data=payload)
+print(r.json())
+
+
+input("Press Enter for the next")
+
+payload = {"value": "dir()", "mode": "amount", "replace_with": "--original--"}
+print("The payload", payload)
+r = requests.post(BASE, data=payload)
+print(r.json())
 
 print("Fin")
